5d034f84958318f0bf925fe313f20b60122bd179
GERARDJM018 <e0985922@u.nus.edu>
2023-06-25 17:07:58 +0700

    animation ms2

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ animate();
 
 function init() {
     scene = new THREE.Scene();
-
+    
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
 
-    geometry = new THREE.BoxGeometry(1, 1, 1);
-    material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+
+    var loader = new THREE.GLTFLoader();
+    loader.load('/room/scene.gltf', function (gltf) {
+        scene.add(gltf.scene);
+      }, undefined, function (error) {
+        console.error(error);
+      });
+    
+
+
 }
 
 function animate() {
